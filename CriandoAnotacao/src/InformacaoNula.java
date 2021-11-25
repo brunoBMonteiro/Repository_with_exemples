ce12d79be1fe0d49c10f15cc04326e865b1e9ff8
Bruno <brunobilheri@hotmail.com>
2021-11-25 14:55:24 -0300

    Criando anotação

---
+++ CriandoAnotacao/src/InformacaoNula.java
@@ -0,0 +1,10 @@
+@interface InformacaoNula {
+
+    String author();
+    int alunoNumero();
+
+    String github() default "http://github.com";
+    String site() default "http://instagram.com";
+
+
+}
---
+++ CriandoAnotacao/src/TesteAnnotation.java
@@ -0,0 +1,10 @@
+public class TesteAnnotation {
+
+    @InformacaoNula(
+            author = "Bruno B Monteiro",
+            alunoNumero = 26
+    )
+    public static void main(String[] args) {
+
+    }
+}
