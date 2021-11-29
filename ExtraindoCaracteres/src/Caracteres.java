69a93896c7252c5fbf8af9f9391deb9698c03e4c
Bruno <brunobilheri@hotmail.com>
2021-11-29 13:48:56 -0300

    Manipulando caracteres

---
+++ ExtraindoCaracteres/src/Caracteres.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Caracteres {
+    public static void main(String[] args) {
+
+        String java = "Java";
+
+        for(int i=0; i < java.length(); i++){
+            System.out.println(java.charAt(i));
+        }
+
+        char[] jav = new char[3];
+        java.getChars(0, 3, jav, 0);
+        System.out.println(jav);
+
+        //seria o mesmo que
+        for(int i = 0, j =0; i<3; i++, j++){
+            jav[j] = java.charAt(i);
+        }
+        System.out.println(jav);
+
+        byte[] javBytes = new byte[3];
+        java.getBytes(0, 3, javBytes, 0);
+        System.out.println(Arrays.toString(javBytes));
+
+        char[] javaChars= java.toCharArray();
+        System.out.println(javaChars);
+    }
+}
