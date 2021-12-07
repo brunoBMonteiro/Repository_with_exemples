4bc2876189d2e1ca9eb7336abf1498c275a2d954
Bruno <brunobilheri@hotmail.com>
2021-12-07 10:21:47 -0300

    classe Thread

---
+++ Threads/src/MinhaThread.java
@@ -0,0 +1,24 @@
+public class MinhaThread extends Thread{
+
+    private String nome;
+    private int tempo;
+
+    public MinhaThread(String nome, int tempo){
+        this.nome = nome;
+        this.tempo = tempo;
+        start();
+    }
+
+    public void run(){
+       try{
+           for(int i = 0; i < 6; i++){
+               System.out.println(nome + " contador " + i);
+               // 500 milisegundos, meio segundo
+               Thread.sleep(tempo);
+           }
+       }catch (InterruptedException e){
+           e.printStackTrace();
+       }
+        System.out.println(nome + " terminou a execução");
+    }
+}
---
+++ Threads/src/TesteThread.java
@@ -0,0 +1,9 @@
+public class TesteThread {
+    public static void main(String[] args) {
+        MinhaThread thread = new MinhaThread("Thread #1", 300);
+
+        MinhaThread thread2 = new MinhaThread("Thread #2", 900);
+        // para não ficar chamando o método toda hora, eu coloco ele no construtor
+        //thread.start();
+    }
+}
